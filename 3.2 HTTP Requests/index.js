aca93e4c822e7f0d8c6ea52527f79596fa2e21db
Ângelo Matins Finassi <159829035+AngeloFinassi@users.noreply.github.com>
2025-09-29 19:24:56 -0300

    update

--- 3.2 HTTP Requests/index.js
+++ 3.2 HTTP Requests/index.js
@@ -1,19 +1,22 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import express from "express"
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
+app.get("/", (req, res) =>{
+    res.send("<h1>Hello Word</h1")
+})
+
+app.listen(3000, () => {
+    console.log("The server is running at pot 3000")
+})
 
 app.get("/about", (req, res) => {
+  // O servidor responde enviando um texto em formato HTML para o navegador.
   res.send("<h1>About Me</h1><p>My name is Angela</p>");
 });
 
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
-});
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+// Esta parte define o que acontece quando alguém acessa a página "/contact"
+app.get("/contact", (req, res) => {
+  // O servidor responde com outro texto em HTML.
+  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
 });
