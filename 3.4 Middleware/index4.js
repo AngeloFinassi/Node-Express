6bd88eab7467c79832da5f20203f2583fbfe1202
Ângelo Martins Finassi <159829035+AngeloFinassi@users.noreply.github.com>
2025-09-30 19:44:53 -0300

    middleware final update (for now)

--- 3.4 Middleware/index4.js
+++ 3.4 Middleware/index4.js
@@ -1,8 +1,31 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+var bandName = ""
+
+app.use(express.urlencoded({ extended: true }));
+
+function bandNameGenerator(req, res, next){
+  bandName = req.body["street"] + req.body["pet"]
+  next()
+}
+
+//roda toda vez
+// app.use(bandNameGenerator)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+})
+
+//so roda bandgenerator quando o post é chamado
+app.post("/submit", bandNameGenerator, (req, res) => {
+  res.send(`<h1>This is your band name:</h1><h2>${bandName}✌️</h2>`)
+})
--- 3.5 Secrets Project/index.js
+++ 3.5 Secrets Project/index.js
@@ -1,3 +1,39 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
+//The password is ILove
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+var userAuth = false
+
+function checkPassword(req, res, next){
+    if (req.body["password"] === "ILove") {
+        userAuth = true
+    }
+    next()
+}
+
+app.post("/check", checkPassword, (req, res) => {
+    if (userAuth) {
+        res.sendFile(__dirname + "/public/secret.html")
+        userAuth = false
+    }
+    else{
+        res.sendFile(__dirname + "/public/index.html");
+    }
+})
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
